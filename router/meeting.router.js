3c8135ef378c68f688af62751972095c4be747be
IBlitzI <baskin0166@hotmail.com>
2025-05-24 21:47:41 +0300

    imp

--- router/meeting.router.js
+++ router/meeting.router.js
@@ -1,13 +1,10 @@
-
 const express = require("express");
 const router = express.Router();
-const controller = require("../controller/meeting.controller");
-const authMiddleware = require("../middlewares/auth.middleware");
 
-router.post("/", authMiddleware(), controller.createMeeting);
-router.put("/:meetingId", authMiddleware(), controller.updateMeeting);
-router.delete("/:meetingId", authMiddleware(), controller.deleteMeeting);
-router.get("/", authMiddleware(), controller.getMeetings);
-router.get("/:meetingId", authMiddleware(), controller.getMeetingById);
+// Meeting routes dosyasını import et
+const meetingRoutes = require("../routes/meetings");
+
+// Tüm meeting routes'ları kullan
+router.use("/", meetingRoutes);
 
 module.exports = router;
